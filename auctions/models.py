82296e67c289ac90c3607a3cb0954f891cfd3140
thenatural86 <philip.smalls86@gmail.com>
2020-10-18 14:07:50 -0400

    user model

--- auctions/models.py
+++ auctions/models.py
@@ -4,3 +4,10 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+
+class Listing(models.Model):
+    title = models.CharField(max_length=64)
+    price = models.IntegerField()
+    image = models.TextField()
+    category = models.CharField(max_length=64)
