df4c2a7bc5bc59d2ce2d0b219522d1fe37aa6e1d
thenatural86 <philip.smalls86@gmail.com>
2020-10-18 14:20:31 -0400

    bid model

--- auctions/models.py
+++ auctions/models.py
@@ -5,9 +5,18 @@ from django.db import models
 class User(AbstractUser):
     pass
 
+# do these classes need id's?
+
 
 class Listing(models.Model):
     title = models.CharField(max_length=64)
+    description = models.TextField()
     price = models.IntegerField()
     image = models.TextField()
     category = models.CharField(max_length=64)
+
+
+class Bid(models.Model):
+    user = models.CharField(max_length=64)
+    title = models.CharField(max_length=64)
+    bid = models.IntegerField()
