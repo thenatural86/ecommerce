0444395b3863d1c77940efde2cfd0d7485fcbd0f
thenatural86 <philip.smalls86@gmail.com>
2020-10-19 21:23:59 -0400

    index routet

--- auctions/admin.py
+++ auctions/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from .models import User, Listing, Bid, Comment
 
+
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "description", "price", "image", "category")
+
+
 # Register your models here.
 admin.site.register(User)
-admin.site.register(Listing)
+admin.site.register(Listing, ListingAdmin)
 admin.site.register(Bid)
 admin.site.register(Comment)
--- auctions/views.py
+++ auctions/views.py
@@ -4,11 +4,14 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 
-from .models import User
+from .models import User, Listing
 
 
 def index(request):
-    return render(request, "auctions/index.html")
+    print()
+    return render(request, "auctions/index.html", {
+        "listings": Listing.objects.all()
+    })
 
 
 def create_listing(request):
